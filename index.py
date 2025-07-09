c29e3417be2c623f859aa226e9f39d8e25e1dfbe
Ishaan Joshi <121894115+IshaanJ2525@users.noreply.github.com>
2025-07-10 01:11:04 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,29 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(
-    page_title="PrognosAir 3D Viewer",
-    layout="wide",
-    initial_sidebar_state="collapsed"
-)
+st.set_page_config(page_title="PrognosAir Viewer", layout="wide")
 
-st.markdown("""
-    <style>
-        iframe {
-            height: 100vh;
-            width: 100vw;
-            border: none;
-        }
-        .block-container {
-            padding: 0;
-        }
-    </style>
-""", unsafe_allow_html=True)
+with open("prognosair.html", "r", encoding="utf-8") as f:
+    html = f.read()
 
-st.title("PrognosAir - 3D Aircraft Maintenance Viewer")
-
-# Embed the full HTML app into the Streamlit app
-with open("prognosair.html", "r") as f:
-    html_string = f.read()
-
-components.html(html_string, height=800, width=1400, scrolling=False)
+components.html(html, height=800, scrolling=False)
